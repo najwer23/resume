39eeba080851c94a47103797be2338b87ac02ee7
Mariusz Najwer <najwer23@gmail.com>
2024-08-19 13:38:10 +0200

    ReactGA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { RouterHash } from './router/RouterHash';
 import { LangProvider } from './translation/useTranslationContext';
 
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-4RB2P61JPP');
+
 export const App = () => {
   return (
     <LangProvider>

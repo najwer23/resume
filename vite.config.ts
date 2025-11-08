14ff7ca7ac44f982b23981f28bceea96b9adc3fd
Mariusz Najwer <najwer23@gmail.com>
2025-11-08 23:07:37 +0100

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/resume/',
   plugins: [
-    react(),
-
     VitePWA({
       scope: '/resume/',
       registerType: 'autoUpdate',

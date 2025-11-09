77f78120f9d3c692e5e0f8531d650a65bb4a1d7d
Mariusz Najwer <najwer23@gmail.com>
2025-11-09 12:06:02 +0100

    feat: add Education and Header components with styles; update Home component to include Education

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import { PaperA4 } from '@resume/app/paperA4/PaperA4';
+import { Education } from '@resume/app/sections/education/Education';
 import { Intro } from '@resume/app/sections/intro/Intro';
 
 export const Home: React.FC<{}> = () => {
   return (
     <PaperA4>
       <Intro />
+      <Education />
     </PaperA4>
   );
 };

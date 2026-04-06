07b8f487e891eeb97f5c5b24e0fb7053836d1490
Mariusz Najwer <najwer23@gmail.com>
2026-04-06 22:14:05 +0200

    Removed google analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ReactGA from 'react-ga4';
 import { LangProvider } from '../translation/useTranslationContext';
 import { RouterHash } from './router/RouterHash';
-
-ReactGA.initialize('G-4RB2P61JPP');
 
 const queryClient = new QueryClient();
 

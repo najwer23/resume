225896dde9e07e38f293db7443b75059a155247d
Mariusz Najwer <najwer23@gmail.com>
2026-07-16 11:42:11 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/pages/home/Page3.tsx
+++ src/app/pages/home/Page3.tsx
@@ -1,12 +1,14 @@
 import { PaperA4 } from '@app/paperA4/PaperA4';
 import { API } from '@app/sections/API';
 import { BlogPostCreator } from '@app/sections/BlogPostCreator';
+import { NestNext } from '@app/sections/NestNext';
 
 export const Page3: React.FC<{}> = () => {
   return (
     <PaperA4>
       <BlogPostCreator />
       <API />
+      <NestNext />
     </PaperA4>
   );
 };
---
+++ src/app/sections/NestNext.tsx
@@ -0,0 +1,43 @@
+import { Grid } from 'najwer23morsels/lib/Grid';
+import { TextBox } from 'najwer23morsels/lib/TextBox';
+import { T } from '../translation/T';
+
+export const NestNext: React.FC<{}> = ({}) => {
+  return (
+    <Grid widthMax={'1400px'} layout="flex" justifyContent="flex-start" margin={'15px 0 0 0'}>
+      <Grid widthMin={'70px'} layout="container" margin={'4px 20px 0 0'}></Grid>
+
+      <Grid widthMax={'700px'} layout="container" margin={0}>
+        <TextBox color="black" mobileSize={18} desktopSize={18} fontWeight={500} tag="h3">
+          USER MANAGEMENT & ANALYTICS PLATFORM
+        </TextBox>
+        <TextBox color="black" mobileSize={12} desktopSize={12} fontWeight={400} tag="h3">
+          (TypeScript, Node.js, NestJS, Prisma, PostgreSQL, Next.js, Tailwind CSS, Swagger, Jest, Vitest)
+        </TextBox>
+        <TextBox
+          color="black"
+          mobileSize={14}
+          desktopSize={14}
+          fontWeight={400}
+          tag="p"
+          margin={'10px 0 5px 0'}
+          textWrap="pretty"
+        >
+          {T('NestNext_Description')}
+        </TextBox>
+
+        <TextBox
+          href="https://github.com/najwer23/nest-next"
+          mobileSize={14}
+          desktopSize={14}
+          color="orangered"
+          colorHover="orangered"
+          rel="noreferrer"
+          target="_blank"
+        >
+          https://github.com/najwer23/nest-next
+        </TextBox>
+      </Grid>
+    </Grid>
+  );
+};
